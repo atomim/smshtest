e4c24d37d507ac81fe4a94bdd52d493fabc1f278
atomim <atomimalli@gmail.com>
2023-03-15 21:50:44 +0200

    Add BG Fades, start adding a metasprite

--- attributes.c
+++ attributes.c
@@ -6,6 +6,9 @@ for the nametable. We copy it from an array in ROM to video RAM.
 #include <string.h>
 #include <stdlib.h>
 
+// include CC65 NES Header (PPU)
+#include <nes.h>
+
 // link the pattern table into CHR ROM
 //#link "chr_generic.s"
 
@@ -30,39 +33,71 @@ const char PALETTE[33] = {
   0x0F,0x13,0x10,0x00,	// background palette 2
   0x0F,0x16,0x10,0x00,       // background palette 3
     
-  0x0D,0x35,0x20,0x00,	// sprite palette 0
-  0x0D,0x37,0x20,0x00,	// sprite palette 1
-  0x0D,0x2D,0x20,0x00,	// sprite palette 2
+  0x0D,0x17,0x20,0x00,	// sprite palette 0
+  0x0D,0x17,0x20,0x00,	// sprite palette 1
+  0x0D,0x17,0x20,0x00,	// sprite palette 2
   0x0D,0x27,0x20,0x00	// sprite palette 3
 
 };
+
+#define TILE 0xd8
+#define ATTR 0
+
+// define a 2x2 metasprite
+const unsigned char metasprite[]={
+        0,      0,      TILE+0,   ATTR, 
+        0,      8,      TILE+1,   ATTR, 
+        8,      0,      TILE+2,   ATTR, 
+        8,      8,      TILE+3,   ATTR, 
+        128};
 
 void p(byte type, byte x, byte y, byte len)
 {
   vram_adr(NTADR_A(x,y));
   vram_fill(0x90+type, len);
-  
+  vram_adr(NTADR_A(x+1,y+1));
+  vram_fill(0x05, 1);
+  vram_fill(0x04, len-4);
+  vram_fill(0x06, 1);
 }
+
+#define NUM_ACTORS 1
+
+byte actor_x[NUM_ACTORS];
+byte actor_y[NUM_ACTORS];
 
 // main function, run after console reset
 void main(void) {
   // set background palette colors
-  pal_bg(PALETTE);
+  pal_all(PALETTE);
+
+  // Place the player
+  actor_x[0]=60;
+  actor_y[0]=143;
   
-  // fill nametable with diamonds
-  //vram_adr(NAMETABLE_A);	// start address ($2000)
-  //vram_fill(0x80, 32*30);	// fill nametable (960 bytes)
   
-  //vram_adr(NTADR_A(20,15));
-  //vram_fill(0x90, 500);
+  // Draw bg fades
+  vram_adr(NAMETABLE_A);
+  vram_fill(0x04, 96); // Sky
+  vram_fill(0x05, 96);
+  vram_adr(NTADR_A(0,22));
+  vram_fill(0x07, 10);  // Bottom left 1
+  vram_fill(0x04, 12);  // Bottom center 1
+  vram_fill(0x07, 9);   // Bottom right 1
+  vram_fill(0x08, 11);  // Bottom left 2
+  vram_fill(0x04, 12);  // Bottom center 2
+  vram_fill(0x08, 11);  // Bottom right 2
+  vram_fill(0x04, 32*5);// Bottom dark
   
+  
+  // Draw platforms BG
   p(0,8,20,16);
   p(1,10,17,4);
   p(1,18,17,4);
   p(1,14,14,4);
   
+  // Set BG colors
   vram_adr(0x23C0);
-  
   // copy attribute table from PRG ROM to VRAM
   vram_write(ATTRIBUTE_TABLE, sizeof(ATTRIBUTE_TABLE));
 
@@ -70,5 +105,21 @@ void main(void) {
   ppu_on_all();
 
   // infinite loop
-  while (1) { }
+  while (1) {
+    char i; // actor index
+    char oam_id; // sprite ID
+    // start with OAMid/sprite 0
+    oam_id = 0;
+    // draw and move all actors
+    for (i=0; i<NUM_ACTORS; i++) {
+      oam_id = oam_meta_spr(actor_x[i], actor_y[i], oam_id, metasprite);
+      //actor_x[i] += actor_dx[i];
+      //actor_y[i] += actor_dy[i];
+    }
+    // hide rest of sprites
+    // if we haven't wrapped oam_id around to 0
+    if (oam_id!=0) oam_hide_rest(oam_id);
+    // wait for next frame
+    ppu_wait_frame();
+  }
 }
